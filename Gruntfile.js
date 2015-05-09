0c8712b2060ed01c3f91284a2ccf79bc29d6cf96
Jeremy Wagner <jeremy.l.wagner@gmail.com>
2015-05-09 14:30:31 -0500

    Created custom theme file.

---
+++ Gruntfile.js
@@ -0,0 +1,33 @@
+module.exports = function(grunt){
+	// Load our tasks
+	grunt.loadNpmTasks("grunt-contrib-watch");
+	grunt.loadNpmTasks("grunt-contrib-sass");
+
+	// Task Configuration
+	var taskConfig = {
+		pkg: grunt.file.readJSON("package.json"),
+		watch:{
+			files: ["index.html", "css/theme/source/wagnasty.scss"],
+			tasks: ["sass"],
+			options:{
+				livereload: true
+			}
+		},
+		sass:{
+			options:{
+				style: "compressed"
+			},
+			dist:{
+				files:{
+					"css/theme/wagnasty.css": "css/theme/source/wagnasty.scss"
+				}
+			}
+		}
+	};
+
+	// Initialize configuration
+	grunt.initConfig(taskConfig);
+
+	// Register the default task
+	grunt.registerTask("default", ["sass", "watch"]);
+};
