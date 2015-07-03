1a1082809e06999b6ac48676fc9d2bf73f4dc57c
Jeremy Wagner <jeremy.l.wagner@gmail.com>
2015-07-03 12:52:19 -0500

    Added jsbin to project.

---
+++ jsbin_/public/js/account/account-settings.js
@@ -0,0 +1,43 @@
+(function(){
+  'use strict';
+
+  /* globals $ */
+
+  $('form.login').submit(function (event) {
+    event.preventDefault();
+
+    var form = $(this);
+    var name = form.find('input[name=username]').val();
+    var key = form.find('input[name=password]').val();
+    var email = form.find('input[name=email]').val();
+    var subscribed = form.find('input[name=subscribed]').prop('checked');
+    var beta = form.find('input[name=beta]').prop('checked');
+    var $loginFeedback = form.find('.loginFeedback');
+    var $csrf = $('#_csrf');
+
+    $loginFeedback.show().text('Checking...');
+
+    $.ajax({
+      url: form.attr('action'),
+      data: {
+        username: name,
+        key: key,
+        email: email,
+        subscribed: subscribed,
+        beta: beta,
+        _csrf: $csrf.val()
+      },
+      type: 'POST',
+      dataType: 'json',
+      complete: function (jqXHR) {
+        var data = $.parseJSON(jqXHR.responseText) || {};
+        if (jqXHR.status === 200) {
+          if (data.message) {
+            $loginFeedback.text(data.message);
+          }
+        }
+      }
+    });
+  });
+
+})();
